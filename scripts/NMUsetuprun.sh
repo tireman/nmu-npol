539d9504ac7865f2ca16aa1a451d626b8fcb045e
tireman <tireman@nmu.edu>
2016-03-03 16:06:11 -0500

    Minor changes to npol.mac, NMUsetuprun.sh, and NpolHBender for current runs.

--- scripts/NMUsetuprun.sh
+++ scripts/NMUsetuprun.sh
@@ -2,7 +2,7 @@
 
 export BUILD_DIR=/home/tireman/simulation/jlab/npol/nmu-npol/build
 export NPOLBASENAME=test_1_Lead10cm_4.4GeV_4Bdl
-export NPOLEVENTSPERFILE=100000
+export NPOLEVENTSPERFILE=500000
 export NPOLDIR=/data3/cgen/NMUSimData/4.4GeV/4Bdl/Lead10cm
 
 if [ ! -e $NPOLDIR ]
