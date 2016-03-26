86a65fcd9458d032a4041ac8352ded110f10ef92
tireman <tireman@nmu.edu>
2016-03-26 13:24:14 -0400

    Minor updates to the NMU scripts

--- scripts/NMUsetupAnalysis.sh
+++ scripts/NMUsetupAnalysis.sh
@@ -5,9 +5,9 @@ export Lead=15
 export Energy=4.4
 export Bfield=4
 export BUILD_DIR=/home/tireman/simulation/jlab/npol/nmu-npol/build
-export NPOLBASENAME=semenov
-export NPOLDIR=/data3/cgen/SemenovGeometry/4.4GeV/4Bdl/Lead15cm
-export NPOLWORKDIR=/data3/cgen/SemenovGeometry/4.4GeV/4Bdl/Lead15cm
+export NPOLBASENAME=npol
+export NPOLDIR=/data2/cgen/NMUSimData/4.4GeV/4Bdl/Lead15cm
+export NPOLWORKDIR=/data2/cgen/NMUSimData/4.4GeV/4Bdl/Lead15cm
 
 export OutputDir=$NPOLDIR/Plots/
 export InputDir=$NPOLDIR/root/
