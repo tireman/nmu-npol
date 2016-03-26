a44ec119ab192fc6fb7593ba22036801c673ddf1
tireman <tireman@nmu.edu>
2016-03-26 17:48:57 -0400

    Minor change to analysis setup script to match file naming scheme.

--- scripts/NMUsetupAnalysis.sh
+++ scripts/NMUsetupAnalysis.sh
@@ -9,10 +9,10 @@ export NPOLBASENAME=npol
 export NPOLDIR=/data2/cgen/NMUSimData/4.4GeV/4Bdl/Lead15cm
 export NPOLWORKDIR=/data2/cgen/NMUSimData/4.4GeV/4Bdl/Lead15cm
 
-export OutputDir=$NPOLDIR/Plots/
-export InputDir=$NPOLDIR/root/
-export WorkOutputDir=$NPOLWORKDIR/Plots/
-export WorkInputDir=$NPOLWORKDIR/root/
+export OutputDir=$NPOLDIR/root
+export InputDir=$NPOLDIR/root
+export WorkOutputDir=$NPOLWORKDIR/Plots
+export WorkInputDir=$NPOLWORKDIR/root
 
 if [ ! -e $NPOLDIR ]
 then
