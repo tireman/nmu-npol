54e0ef20d009337270bac0b249e68a2899ea07ec
Daniel Wilbern <dwilbern@nmu.edu>
2016-06-15 15:50:55 -0400

    Rework of dE versus E script.

--- include/NpolDetectorEvent.hh
+++ include/NpolDetectorEvent.hh
@@ -15,22 +15,32 @@
 
 class NpolDetectorEvent : public TObject {
 
-public:
-  
-  inline NpolDetectorEvent() { }
-  virtual ~NpolDetectorEvent() { }
+	public:
 
-  double gPosX;
-  double gPosY;
-  double gPosZ;
-  double lPosX;
-  double lPosY;
-  double lPosZ;
-  double time;
-  double totEnergyDep;
-  bool thresholdExceeded;
+		NpolDetectorEvent() {
+			gPosX = 0.0;
+			gPosY = 0.0;
+			gPosZ = 0.0;
+			lPosX = 0.0;
+			lPosY = 0.0;
+			lPosZ = 0.0;
+			time = 0.0;
+			thresholdExceeded = false;
+			totEnergyDep = 0.0;
+		}
+		~NpolDetectorEvent() { }
 
-  ClassDef(NpolDetectorEvent, 1);
+		double gPosX;
+		double gPosY;
+		double gPosZ;
+		double lPosX;
+		double lPosY;
+		double lPosZ;
+		double time;
+		double totEnergyDep;
+		bool thresholdExceeded;
+
+		ClassDef(NpolDetectorEvent, 1);
 };
 
 #endif
--- root/MakeNpolClasses.sh
+++ root/MakeNpolClasses.sh
@@ -7,5 +7,5 @@ cp ../include/NpolStatistics.hh .
 cp ../include/NpolDetectorEvent.hh .
 cp ../include/linkdef.hh .
 rootcling -f dict.cxx -c NpolVertex.hh NpolTagger.hh NpolStatistics.hh NpolStep.hh NpolDetectorEvent.hh linkdef.hh
-g++ -std=c++11 -Wall -shared -fPIC -I/opt/root/root-6.06.02/include/root -o NpolClass.so dict.cxx
+g++ -std=c++11 -Wall -shared -fPIC -I$ROOTSYS/include/root -o NpolClass.so dict.cxx
 
