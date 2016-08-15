8a28ffcb31e61f3a08b0f58d5d19414b20400476
tireman <tireman@nmu.edu>
2016-08-15 13:14:41 -0400

    Minor changes to the analysis enivornmental setup scripts.

--- analysis/envscripts/NMUsetupAnalysis.sh
+++ analysis/envscripts/NMUsetupAnalysis.sh
@@ -5,13 +5,13 @@ export Lead=15
 export Energy=4.4
 export Bfield=4
 export BUILD_DIR=/home/tireman/simulation/jlab/npol/nmu-npol/build/simulation
-export NPOLBASENAME=electronBeam
-#export NPOLDIR=/data1/cgen/NeutronOnly/PointSource/10cmScint
+export NPOLBASENAME=targetGamma
+export NPOLDIR=/data1/cgen/TargetTaggerBiasRuns/shortTest
 #export NPOLDIR=/home/tireman/simulation/jlab/npol/analysis/test2
-export NPOLDIR=/data2/cgen/JlabSimData/Summer2016Run/NoNpolArm/4.4GeV
+#export NPOLDIR=/data2/cgen/JlabSimData/Summer2016Run/NoNpolArm/4.4GeV
 
-#export NPOLWORKDIR=/data1/cgen/NeutronOnly/PointSource/10cmScint
-export NPOLWORKDIR=/data2/cgen/JlabSimData/Summer2016Run/NoNpolArm/4.4GeV
+export NPOLWORKDIR=/data1/cgen/TargetTaggerBiasRuns/shortTest
+#export NPOLWORKDIR=/data2/cgen/JlabSimData/Summer2016Run/NoNpolArm/4.4GeV
 
 export OutputDir=$NPOLDIR/histos
 export InputDir=$NPOLDIR/root
--- simulation/scripts/NMUsetuprun.sh
+++ simulation/scripts/NMUsetuprun.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 export BUILD_DIR=/home/tireman/simulation/jlab/npol/nmu-npol/build/simulation
-export NPOLBASENAME=npol_Lead15cm_4.4GeV_4Bdl
-export NPOLEVENTSPERFILE=200000
-export NPOLDIR=/data1/cgen/NeutronOnly/PointSource/10cmScint
+export NPOLBASENAME=targetGamma_Lead15cm_4.4GeV_4Bdl
+export NPOLEVENTSPERFILE=100000
+export NPOLDIR=/data1/cgen/TargetTaggerBiasRuns/shortTest
 #export NPOLDIR=/home/tireman/simulation/jlab/npol/analysis/test
-export NPOLWORKDIR=/data1/cgen/NeutronOnly/PointSource/10cmScint
+export NPOLWORKDIR=/data1/cgen/TargetTaggerBiasRuns/shortTest
 #export NPOLWORKDIR=/home/tireman/simulation/jlab/npol/analysis/test
 
 if [ ! -e $NPOLDIR ]
