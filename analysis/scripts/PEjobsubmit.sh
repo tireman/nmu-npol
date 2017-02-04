b43f3d6dd6eaba9512ea9318590c3964df15bcb6
W Tireman <tireman@krieger.nmu.edu>
2017-02-04 08:40:33 -0500

    Added a loop such it can start several jobs from one script that run over a fixed number of run numbers.

--- analysis/scripts/PEjobsubmit.sh
+++ analysis/scripts/PEjobsubmit.sh
@@ -4,7 +4,7 @@ for ((i=$1; i<=$2; i++))
 do
 
     export JOBNUMBER=$i
-	$BUILD_DIR/../analysis/NpolAnalysis
+	$BUILD_DIR/../analysis/NpolAnalysis 
     #root -b -q $BUILD_DIR/../../analysis/scripts/ProcessElectrons.cxx+
 
 done
