10563dec9127efeec1eef1dcacffa68bbf43db9b
tireman <tireman@nmu.edu>
2017-02-18 09:40:27 -0500

    Updated license in the file.

--- simulation/src/NpolTrackingAction.cc
+++ simulation/src/NpolTrackingAction.cc
@@ -1,3 +1,15 @@
+//********************************************************************
+//* License and Disclaimer: From GEANT Collaboration                 *
+//*                                                                  *
+//* The  Geant4 software  is  copyright of the Copyright Holders  of *
+//* the Geant4 Collaboration.  It is provided  under  the terms  and *
+//* conditions of the Geant4 Software License,  included in the file *
+//* LICENSE and available at  http://cern.ch/geant4/license .  These *
+//* include a list of copyright holders.     		      	*
+//********************************************************************
+
+// The tracking action was creaded to assist with recording tracks.
+// Created: Daniel Wilbern September 2015
 
 #include "G4Track.hh"
 
