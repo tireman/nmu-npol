51a024a6df1540896d781eadae1551eee860ad92
tireman <tireman@nmu.edu>
2017-03-10 13:02:42 -0500

    Minor changes to the scripts to handle the setup files correctly.

--- simulation/scripts/NMUPhysics/NMUsetuprun.sh
+++ simulation/scripts/NMUPhysics/NMUsetuprun.sh
@@ -3,11 +3,13 @@
 export Lead=15
 export Energy=4.4
 export Bfield=4
-export BUILD_DIR=/home/tireman/simulation/jlab/npol/nmu-npol/build/simulation
-export NPOLBASENAME=sourceMuon+_Lead$Lead\cm_$Energy\GeV_$Bfield\Bdl
+export PType=Proton
+
+export BUILD_DIR=/home/tireman/simulation/jlab/nmu-npol/build/simulation
+export NPOLBASENAME=source$PType\_Lead$Lead\cm_$Energy\GeV_$Bfield\Bdl
 export NPOLEVENTSPERFILE=100000
-export NPOLDIR=/data2/cgen/JlabSimData/December2016/TargetTaggerSource/$Energy\GeV/$Bfield\Bdl/TargetTagger56by21
-export NPOLWORKDIR=/data2/cgen/JlabSimData/December2016/TargetTaggerSource/$Energy\GeV/$Bfield\Bdl/TargetTagger56by21
+export NPOLDIR=/home/tireman/data1/TargetTaggerSource/$Energy\GeV/$Bfield\Bdl
+export NPOLWORKDIR=/home/tireman/data1/TargetTaggerSource/$Energy\GeV/$Bfield\Bdl
 export RawDataDir=$NPOLDIR/root
 export HistoOutputDir=$NPOLDIR/histos
 
