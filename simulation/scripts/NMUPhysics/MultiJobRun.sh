4844658181aa6b65a33df8a585b009bfb78efc26
tireman <tireman@nmu.edu>
2017-05-13 07:57:50 -0400

    Added individualized scripts to NMUPhysics scripts so I can launch long runs on a Physics server (Physics 3, Krieger, etc) that run over individualized biasing runs for the 9 different particle speices.

---
+++ simulation/scripts/NMUPhysics/MultiJobRun.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+#source /home/tireman/simulation/jlab/nmu-npol/build/simulation/scripts/NMUPhysics/NMUsetuprun.sh
+
+./scripts/NMUPhysics/NMUjobsubmitPion+.sh 1 10 &
+./scripts/NMUPhysics/NMUjobsubmitPion-.sh 1 10 &
+
+./scripts/NMUPhysics/NMUjobsubmitMuon+.sh 1 10 &
+./scripts/NMUPhysics/NMUjobsubmitMuon-.sh 1 10 &
+
+./scripts/NMUPhysics/NMUjobsubmitPositron.sh 1 10 &
+
+./scripts/NMUPhysics/NMUjobsubmitProton.sh 1 10 &
+
+./scripts/NMUPhysics/NMUjobsubmitNeutron.sh 1 5 &
+./scripts/NMUPhysics/NMUjobsubmitNeutron.sh 6 5 &
+
+./scripts/NMUPhysics/NMUjobsubmitElectron.sh 1 2 &
+./scripts/NMUPhysics/NMUjobsubmitElectron.sh 3 2 &
+./scripts/NMUPhysics/NMUjobsubmitElectron.sh 5 2 &
+./scripts/NMUPhysics/NMUjobsubmitElectron.sh 7 2 &
+./scripts/NMUPhysics/NMUjobsubmitElectron.sh 9 2 &
+./scripts/NMUPhysics/NMUjobsubmitElectron.sh 11 2 &
+./scripts/NMUPhysics/NMUjobsubmitElectron.sh 13 2 &
+./scripts/NMUPhysics/NMUjobsubmitElectron.sh 15 2 &
+./scripts/NMUPhysics/NMUjobsubmitElectron.sh 17 2 &
+./scripts/NMUPhysics/NMUjobsubmitElectron.sh 19 2 &
+
+./scripts/NMUPhysics/NMUjobsubmitGamma.sh 1 2 &
+./scripts/NMUPhysics/NMUjobsubmitGamma.sh 3 2 &
+./scripts/NMUPhysics/NMUjobsubmitGamma.sh 5 2 &
+./scripts/NMUPhysics/NMUjobsubmitGamma.sh 7 2 &
+./scripts/NMUPhysics/NMUjobsubmitGamma.sh 9 2 &
+./scripts/NMUPhysics/NMUjobsubmitGamma.sh 11 2 &
+./scripts/NMUPhysics/NMUjobsubmitGamma.sh 13 2 &
+./scripts/NMUPhysics/NMUjobsubmitGamma.sh 15 2 &
+./scripts/NMUPhysics/NMUjobsubmitGamma.sh 17 2 &
+./scripts/NMUPhysics/NMUjobsubmitGamma.sh 19 2 &
---
+++ simulation/scripts/NMUPhysics/NMUjobsubmitGamma.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]
+then
+	START=1
+	FINISH=1
+elif [ $# -eq 1 ]
+then
+	START=1
+	FINISH=$1
+else
+	START=$1
+	FINISH=$(($1 + $2 - 1))
+fi
+
+for ((i=$START; i<=$FINISH; i++))
+do
+    export JOBNUMBER=$i
+	export PType=Gamma
+	source /home/tireman/simulation/jlab/nmu-npol/build/simulation/scripts/NMUPhysics/NMUsetuprun.sh
+
+    if [ -f "$NPOLDIR/${NPOLBASENAME}_$i.out" ]
+    then
+		rm $NPOLDIR/${NPOLBASENAME}_$i.out
+		rm $NPOLDIR/${NPOLBASENAME}_$i.err
+    fi
+
+	echo "Starting up Job Number $i."
+    $BUILD_DIR/Npolapp $BUILD_DIR/macros/Run$Energy\GeV/ParticleFlux$PType.mac 1>$NPOLDIR/dumpFiles/${NPOLBASENAME}_$i.out 2>$NPOLDIR/dumpFiles/${NPOLBASENAME}_$i.err 
+    
+	hadd -f -k $NPOLDIR/root/source$PType\_Lead$Lead\cm_$Energy\GeV_$Bfield\Bdl_$i.root $NPOLDIR/root/source$PType\_Lead$Lead\cm_$Energy\GeV_$Bfield\Bdl_$i\_*.root
+
+	rm $NPOLDIR/root/source$PType\_Lead$Lead\cm_$Energy\GeV_$Bfield\Bdl_$i\_*.root
+
+done
