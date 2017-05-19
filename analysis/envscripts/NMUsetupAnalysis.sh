8066e495677e936e8f1684852fc32ebac0d3628d
tireman <tireman@nmu.edu>
2017-05-19 12:10:56 -0400

    Added some lines for future runs.

--- analysis/envscripts/NMUsetupAnalysis.sh
+++ analysis/envscripts/NMUsetupAnalysis.sh
@@ -2,15 +2,28 @@
 
 export Lead=15
 export Energy=4.4
-export Bfield=4
-export PType=Gamma
+export Bfield=1
+export PType=Total
 export BUILD_DIR=/home/tireman/simulation/jlab/nmu-npol/build/simulation
 export NPOLLIB_DIR=/home/tireman/simulation/jlab/nmu-npol/build/npollib
-export NPOLBASENAME=source$PType\_Lead$Lead\cm_$Energy\GeV_$Bfield\Bdl 
+export NPOLBASENAME=source$PType\_Lead$Lead\cm_$Energy\GeV_$Bfield\Bdl
 
-export NPOLDIR=/home/tireman/data1/TargetTaggerSource/$Energy\GeV/$Bfield\Bdl
-export NPOLWORKDIR=/home/tireman/data1/TargetTaggerSource/$Energy\GeV/$Bfield\Bdl 
-#/NeutralParticles
+#electronBeam_Lead$Lead\cm_$Energy\GeV_$Bfield\Bdl 
+#source$PType\_Lead$Lead\cm_$Energy\GeV_$Bfield\Bdl 
+
+export NPOLDIR=/home2/tireman-h2/data1/TargetTaggerSource/$Energy\GeV/$Bfield\Bdl/6mSameAcceptance
+
+#/home/tireman/data1/TargetTaggerSource/$Energy\GeV/$Bfield\Bdl
+#/home/tireman/data1/JlabSimData/AprilRun2017/4.4GeV/4Bdl
+#/home/tireman/data1/TargetTaggerSource/$Energy\GeV/17April2017/$Bfield\Bdl/7meterSameAcceptance
+#/home/tireman/data1/TargetTaggerSource/$Energy\GeV/$Bfield\Bdl #/5meterSameAcceptance 
+#/home/tireman/data1/JlabSimData/Feb2017_ElectronRunTest/$Energy\GeV/$Bfield\Bdl/70cmWide26cmTallTargetTagger #/ChargedParticles #/5meterSameAcceptance/NeutralParticles #ChargedParticles 
+  
+export NPOLWORKDIR=/home2/tireman-h2/data1/TargetTaggerSource/$Energy\GeV/$Bfield\Bdl/6mSameAcceptance/ChargedParticles
+
+#/home2/tireman-h2/data1/TargetTaggerSource/$Energy\GeV/$Bfield\Bdl/5mSameAcceptance
+#/home/tireman/data1/TargetTaggerSource/$Energy\GeV/$Bfield\Bdl/OnlyDipoleSurface/AllParticles
+#/home/tireman/data1/JlabSimData/AprilRun2017/4.4GeV/4Bdl/CutFrontWall/AllParticles #LeadWallOnly #/NeutralParticles #/ChargedParticles
 
 export OutputDir=$NPOLDIR/Output
 export InputDir=$NPOLDIR/root
